54f0aa3596e4e25efc543ee373e60a59e7a8c7cc
Шелков Дмитрий <bobyorrr@gmail.com>
2021-02-17 14:35:20 +0300

    + .input, .input_tel-country

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,40 @@
-let text = "Привет Васян";
-console.log(text);
+const activeClass = "active";
+
+// https://imask.js.org/guide.html
+// Маски для телефона
+const maskTel = {
+	rus: {
+		mask: "+{7} (000) 000-00-00",
+	},
+	aze: {
+		mask: "+{994} (000) 000-00-00",
+	},
+};
+
+// Выбор маски для инпута телефона tel-country
+const telCountryList = document.querySelectorAll(".input_tel-country");
+if (telCountryList) {
+	for (const telCountry of telCountryList) {
+		const input = telCountry.querySelector(".input__input"),
+			button = telCountry.querySelector(".tel-country__select"),
+			img = telCountry.querySelector(".tel-country__img"),
+			listFlag = telCountry.querySelectorAll(".tel-country__option-input"),
+			wrap = telCountry.querySelector(".tel-country");
+
+		let mask = IMask(input, maskTel[button.dataset.country]);
+
+		button.addEventListener("click", () => {
+			wrap.classList.toggle(activeClass);
+		});
+
+		for (const flag of listFlag) {
+			flag.addEventListener("change", (event) => {
+				img.src = `./assets/images/flag/flag-${event.currentTarget.value}.svg`;
+				mask.destroy();
+				mask = IMask(input, maskTel[event.currentTarget.value]);
+				button.dataset.country = event.currentTarget.value;
+				wrap.classList.toggle(activeClass);
+			});
+		}
+	}
+}
